0f38794e2090077da664eafaa54afc97367dd886
singh74423 <singhujjwal744@gmail.com>
2024-12-04 08:30:14 +0530

    Initial commit

--- Task3.java
+++ Task3.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Taskm {
+public class Task3 {
 
     public static void main(String[] args) {
         List<String> tasks = new ArrayList<>();
